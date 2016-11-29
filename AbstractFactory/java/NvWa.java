dfe6388cd610f31badee63c7fe7561157b02c54b
zcphoenix2016 <13564240769@139.com>
2016-11-29 11:59:45 +0800

    refactor NvWa.java to be more readable.

--- AbstractFactory/java/NvWa.java
+++ AbstractFactory/java/NvWa.java
@@ -2,14 +2,14 @@ public class NvWa{
     public static void main(String[] args){
         HumanFactory maleHumanFactory = new MaleHumanFactory();
         HumanFactory femaleHumanFactory = new FemaleHumanFactory();
-        
-        System.out.println("FemaleYellowHuman created.");
+
+        System.out.println("--FemaleYellowHuman created--");
         Human femaleYellowHuman = femaleHumanFactory.createYellowHuman();
         femaleYellowHuman.getColor();
         femaleYellowHuman.getSex();
         femaleYellowHuman.talk();
 
-        System.out.println("MaleYellowHuman created.");
+        System.out.println("--MaleYellowHuman created--");
         Human maleYellowHuman = maleHumanFactory.createYellowHuman();
         maleYellowHuman.getColor();
         maleYellowHuman.getSex();
