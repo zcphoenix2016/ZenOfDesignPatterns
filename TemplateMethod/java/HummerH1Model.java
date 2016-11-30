318edfa143ec189592b103c3b87410863a1f8e23
zcphoenix2016 <13564240769@139.com>
2016-11-30 16:18:08 +0800

    Add '@Override' in TemplateMethod implementation.

--- TemplateMethod/java/HummerH1Model.java
+++ TemplateMethod/java/HummerH1Model.java
@@ -1,16 +1,20 @@
 public class HummerH1Model extends HummerModel{
+    @Override
     public void alarm(){
         System.out.println("Hummer-H1 is alarming...");
     }
 
+    @Override
     public void engineBoom(){
         System.out.println("Hummer-H1 engine is booming...");
     }
 
+    @Override
     public void start(){
         System.out.println("Hummer-H1 started.");
     }
 
+    @Override
     public void stop(){
         System.out.println("Hummer-H1 stopped.");
     }
--- TemplateMethod/java/HummerH2Model.java
+++ TemplateMethod/java/HummerH2Model.java
@@ -1,16 +1,20 @@
 public class HummerH2Model extends HummerModel{
+    @Override
     public void alarm(){
         System.out.println("Hummer-H2 is alarming...");
     }
 
+    @Override
     public void engineBoom(){
         System.out.println("Hummer-H2 engine is booming...");
     }
 
+    @Override
     public void start(){
         System.out.println("Hummer-H2 started.");
     }
 
+    @Override
     public void stop(){
         System.out.println("Hummer-H2 stopped.");
     }
